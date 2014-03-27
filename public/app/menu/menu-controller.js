fca82880c07e4e2b502a9af787c2efb340429b7a
adelegue <adelegue@hotmail.com>
2014-03-27 11:54:12 +0100

    Init

--- public/app/menu/menu-controller.js
+++
@@ -1,7 +0,0 @@
-angular.module('marvelapp.menu', []).controller('MenuCtrl', ['$scope', '$location', function ($scope, $location) {
-
-    $scope.isActive = function (viewLocation) {
-        return viewLocation === $location.path();
-    };
-
-}]);
